5c6ec48e1b13cd0e04b276cfcab224268a2538d0
Sean Li <cli049@uottawa.ca>
2024-09-20 21:47:17 -0400

    Linked the signup url with the register.html

--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def signup(request):
-    return HttpResponse("Hello from the Signup app!")
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render())
